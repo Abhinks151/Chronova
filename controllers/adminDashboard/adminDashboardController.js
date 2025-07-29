c541d48f9beea6b975f7ad56c0679fe1de39a1b5
Abhin <abhin1511203@gmail.com>
2025-07-29 12:08:48 +0530

    Implementd and pie chart in admin dashboard and fixed several bugs inorder management in admin side nad resent verification codein user side

--- controllers/adminDashboard/adminDashboardController.js
+++ controllers/adminDashboard/adminDashboardController.js
@@ -1,6 +1,7 @@
 import {
   getAdminDashboardChartService,
-  getAdminDashboardPageDataService
+  getAdminDashboardPageDataService,
+  getAdminPieChartDataService
 } from "../../services/adminDashboardService/adminDashboardService.js";
 import httpStatusCode from "../../utils/httpStatusCode.js";
 import {logger} from '../../config/logger.js';
@@ -46,3 +47,27 @@ export const getAdminDashboardChartData = async (req, res) => {
     res.status(httpStatusCode.INTERNAL_SERVER_ERROR.code).json({ error: error.message });
   }
 }
+
+export const getAdminDashboardPieData = async (req, res) => {
+  try {
+    const { type } = req.params;
+    const { startDate, endDate } = req.query;
+
+    const data = await getAdminPieChartDataService(type, startDate, endDate);
+
+    res.status(httpStatusCode.OK.code).json({
+      success: true,
+      data
+    });
+
+  } catch (err) {
+    logger.error('Pie chart fetch error:', err.message);
+    console.error('Pie chart fetch error:', err.message);
+    const status = err.message.includes('Invalid chart type') ? 400 : 500;
+
+    res.status(status).json({
+      success: false,
+      message: err.message
+    });
+  }
+};
