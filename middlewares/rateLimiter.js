c50756f6b07eb96e2e0c0c2b526f960a4b4bead2
Abhin <abhin1511203@gmail.com>
2025-08-14 11:22:28 +0530

    fix(rate limit) added a new ejs page to show when reate limiter limits also added a fallback for avatar image

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -1,12 +1,34 @@
 import rateLimit from 'express-rate-limit';
 
+// export const limit = rateLimit({
+//   windowMs:1*60*1000,
+//   max:100,
+//   standardHeaders:true,
+//   legacyHeaders:false,
+//   message:{
+//     success:false,
+//     message:'Too many request, Please try again later.'
+//   }
+// })
+
+
 export const limit = rateLimit({
-  windowMs:1*60*1000,
-  max:100,
-  standardHeaders:true,
-  legacyHeaders:false,
-  message:{
-    success:false,
-    message:'Too many request, Please try again later.'
+  windowMs: 1 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req, res, next, options) => {
+    if (req.accepts('html')) {
+      return res.status(options.statusCode).render('Layouts/rate-limit', {
+        title: '429-Too Many Requests',
+        retryAfter: Math.ceil(options.windowMs / 1000),
+      });
+    } else {
+      res.status(options.statusCode).json({
+        success: false,
+        message: 'Too many requests, please try again later.',
+        retryAfter: Math.ceil(options.windowMs / 1000),
+      });
+    }
   }
-})
+});
